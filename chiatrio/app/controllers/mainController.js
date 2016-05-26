309ea266014bd565bd134f57278f858b671faf2a
Tyler Gering <Tyler@Liatrios-MacBook-Pro.local>
2016-05-26 13:11:30 -0700

    got a succesfull function call from the controller

--- chiatrio/app/controllers/mainController.js
+++ chiatrio/app/controllers/mainController.js
@@ -1,10 +1,16 @@
 //File: mainController.js
 
-angular.module('basicTweet')
-.controller('tweetController', function(){
+angular.module('basicTweet', [])
+.controller('tweetController', function() {
 
   this.post = function post(){
     window.alert("SUCCESS");
-  }
+  };
 
+});
+
+
+angular.module('HelloWorldApp', [])
+   .controller('HelloWorldController', function($scope) {
+       $scope.greeting = "Hello World";
 });
