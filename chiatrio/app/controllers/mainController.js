84cffd970e398a4c26da0c57a516fd31055982c3
Tyler Gering <Tyler@Liatrios-MacBook-Pro.local>
2016-05-26 14:45:41 -0700

    CHICO-33 succesfully passing variables back and forth, now working on populating an array that will hold the names and tweets

--- chiatrio/app/controllers/mainController.js
+++ chiatrio/app/controllers/mainController.js
@@ -4,13 +4,12 @@ angular.module('basicTweet', [])
 .controller('tweetController', function() {
 
   this.post = function post(){
-    window.alert("SUCCESS");
-  };
-
-});
-
-
-angular.module('HelloWorldApp', [])
-   .controller('HelloWorldController', function($scope) {
-       $scope.greeting = "Hello World";
+     this.names = [];
+     this.messages = [];
+     this.names.push(this.name);
+     this.messages.push(this.message);
+     for(var i =0; i < this.names.length; i++){
+       window.alert(this.names[i] + " " + this.messages[i] + "\n");
+     }
+   };
 });
